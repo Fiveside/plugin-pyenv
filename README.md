3f8219c83a8ed8b2343c276b415b006360653a3f
Erich Healy <ErichRHealy@gmail.com>
2017-04-06 08:23:31 -0700

    Minor updates, still not ready for use though

--- README.md
+++ README.md
@@ -1,19 +1,20 @@
-<img src="https://cdn.rawgit.com/oh-my-fish/oh-my-fish/e4f1c2e0219a17e2c748b824004c8d0b38055c16/docs/logo.svg" align="left" width="144px" height="144px"/>
-
-#### pyenv
-> A plugin for [Oh My Fish][omf-link].
-
+# plugin-pyenv
 [![MIT License](https://img.shields.io/badge/license-MIT-007EC7.svg?style=flat-square)](/LICENSE)
 [![Fish Shell Version](https://img.shields.io/badge/fish-v2.2.0-007EC7.svg?style=flat-square)](https://fishshell.com)
-[![Oh My Fish Framework](https://img.shields.io/badge/Oh%20My%20Fish-Framework-007EC7.svg?style=flat-square)](https://www.github.com/oh-my-fish/oh-my-fish)
 
-<br/>
+A fish plugin that sets up you up for pyenv.
 
+## Features
+* Looks for pyenv in common places
+* When pyenv is not installed, clobbers the `pyenv` command to tell you how to fix the problem
+* When `PYENV_ROOT` is set, automatically sets up pyenv without the need to reload
+* Hooks in pyenv completions
 
 ## Install
 
+### Using fisherman
 ```fish
-$ omf install pyenv
+$ fisher Fiveside/pyenv
 ```
 
 
@@ -25,13 +26,11 @@ $ pyenv
 
 
 # License
-
 [MIT][mit] © [Erich Healy][author] et [al][contributors]
 
 
 [mit]:            https://opensource.org/licenses/MIT
-[author]:         https://github.com/{{USER}}
-[contributors]:   https://github.com/{{USER}}/plugin-pyenv/graphs/contributors
-[omf-link]:       https://www.github.com/oh-my-fish/oh-my-fish
+[author]:         https://github.com/Fiveside
+[contributors]:   https://github.com/Fiveside/plugin-pyenv/graphs/contributors
 
 [license-badge]:  https://img.shields.io/badge/license-MIT-007EC7.svg?style=flat-square
--- init.fish
+++ init.fish
@@ -1,38 +1,70 @@
 # pyenv initialization hook
-#
-# You can use the following variables in this file:
-# * $package       package name
-# * $path          package path
-# * $dependencies  package dependencies
+# globals: pyenv_plugin_hook_pyenv, pyenv_plugin_initialized
 
 function __pyenv_plugin_pyenv_missing_hook -d "A shadow for the pyenv command if its missing"
-	printf "pyenv is not installed.  Please make sure that PYENV_ROOT is set."
+  echo "pyenv is not installed."
+  echo
+  echo "Ensure sure that PYENV_ROOT is set to pyenv's location or"
+  echo "that pyenv is installed in its default location"
+  echo
+  echo "To install pyenv automatically, run pyenv_install"
+  echo
+end
+
+function pyenv_install -d "Automatically install pyenv for the current user"
+  set -l g "https://github.com/yyuu"
+  set -l root "$HOME/.local/share/pyenv"
+  mkdir -p "$root"
+
+  function checkout -n url dest
+    git clone (printf "https://github.com/yyuu/%s" "$url") (printf "$root/%s" "$dest")
+  end
+
+  checkout "pyenv.git" ""
+  checkout "pyenv-installer.git" "plugins/pyenv-installer"
+  checkout "pyenv-update.git" "plugins/pyenv-update"
+  checkout "pyenv-virtualenv.git" "plugins/pyenv-virtualenv"
+  checkout "pyenv-which-ext.git" "plugins/pyenv-which-ext"
+
+  set -xUg PYENV_ROOT $root
+  echo "TODO!"
 end
 
 function __pyenv_plugin_add_hook -d "Enable shadowing the real pyenv command when its missing"
-	if set -qg pyenv_plugin_hook_pyenv
-		return
-	end
-	set -g pyenv_plugin_hook_pyenv true
-	functions -e pyenv
+  if set -qg pyenv_plugin_hook_pyenv
+    return
+  end
+  set -g pyenv_plugin_hook_pyenv true
+  functions -c __pyenv_plugin_pyenv_missing_hook pyenv
 end
 
 function __pyenv_plugin_remove_hook -d "Remove pyenv shadow function"
-	if not set -qg pyenv_plugin_hook_pyenv
-		return
-	end
-	set -eg pyenv_plugin_hook_pyenv
-	functions -c __pyenv_plugin_pyenv_missing_hook pyenv
+  if not set -qg pyenv_plugin_hook_pyenv
+    return
+  end
+  set -eg pyenv_plugin_hook_pyenv
+  functions -e pyenv
 end
 
 function __pyenv_plugin_on_state_change -d "Pyenv root variable change hook" -v PYENV_ROOT
-	if not type -aq pyenv; and set -q PYENV_ROOT; and test -e $PYENV_ROOT/bin/pyenv
-		__pyenv_plugin_remove_hook
-		set -xg PATH $PYENV_ROOT/bin $PATH
-	else
-		# Pyenv doesn't exist, make sure we've hooked it
-		__pyenv_plugin_add_hook
-	end
+  # is pyenv installed in a default location without setting up pyenv_root?
+  if not set -q PYENV_ROOT; and test -e $HOME/.pyenv/bin/pyenv
+    # Causes a recursive call here.
+    set -xg PYENV_ROOT $HOME/.pyenv/bin/pyenv
+    return
+  end
+
+  if not type -aq pyenv; and set -q PYENV_ROOT; and test -e $PYENV_ROOT/bin/pyenv
+    __pyenv_plugin_remove_hook
+    set -xg PATH $PYENV_ROOT/bin $PATH
+    if status is-interactive
+      source (pyenv init - | psub)
+      source (pyenv virtualenv-init | psub)
+    end
+  else
+    # Pyenv doesn't exist, make sure we've hooked it
+    __pyenv_plugin_add_hook
+  end
 end
 
 __pyenv_plugin_on_state_change
